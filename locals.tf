aefc59936ac9c1dd19c9dcbce23344aaf27f0910
Devavrat Oka <devavratoka@google.com>
2025-10-20 17:59:53 -0600

    proj

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  project_id              = "proj-oka-int-demo"
+  project_id              = var.project_id
   ncc_hub_name            = "ncc-hub-01"
   primary_region          = "us-central1"
   secondary_region        = "us-east4"
@@ -8,4 +8,8 @@ locals {
   primary_region_eu_zonea = "europe-west3-a"
   service_account_email   = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
   service_account_scopes  = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+}
+
+variable "project_id" {
+  type = string
 }
